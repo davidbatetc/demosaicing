5017001533bbc89e4a38701e68be18fe9a422158
davidbatetc <davidbatetc+github@gmail.com>
2023-04-08 13:47:32 +0300

    Replace demosaicMalvar2004 with the fast implementation

--- demosaicMalvar2004.m
+++ demosaicMalvar2004.m
@@ -1,105 +1,100 @@
 function J = demosaicMalvar2004(I, opts)
     assert(strcmp(opts.sensorAlignment, "rggb"),...
            "The given sensor alignment is not implemented.");
-    
-    h = height(I);
-    w = width(I);
+
     sI = single(I);
     J = repmat(sI, [1, 1, 3]);
-    
+
     % Channel indices
     idR = 1;
     idG = 2;
     idB = 3;
 
-    for r = 1:h
-        for c = 1:w
-            refCid = pixelRawColorId(r, c);
-            cids = [1, 2, 3];
-            cids(refCid) = [];
+    K = 5;  % Kernel size. Hardcoded.
+    M = (K - 1)/2;  % Half-kernel size
 
-            for cid = cids
-                weights = getWeights(cid, r, c);
+    % Some padding to not go out of bounds
+    padI = padarray(sI, [M, M]);
 
-                J(r, c, cid) = 0;
-                weightSum = 0;
-                for d1 = -2:2
-                    for d2 = -2:2
-                        if isWithinBounds(r + d1, c + d2, h, w)
-                            weightSum = weightSum + weights(3 + d1, 3 + d2);
-                            J(r, c, cid) = J(r, c, cid)...
-                                + weights(3 + d1, 3 + d2)*sI(r + d1, c + d2);
-                        end
-                    end
-                end
-                J(r, c, cid) = J(r, c, cid)/weightSum;
-            end
-        end
+
+    %% Build the red channel
+    J(1:2:end, 1:2:end, idR) = sI(1:2:end, 1:2:end);
+    J(1:2:end, 2:2:end, idR) = findPixelsInPosition(padI, idR, 0, 1);
+    J(2:2:end, 1:2:end, idR) = findPixelsInPosition(padI, idR, 1, 0);
+    J(2:2:end, 2:2:end, idR) = findPixelsInPosition(padI, idR, 1, 1);
+
+
+    %% Build the green channel
+    J(1:2:end, 1:2:end, idG) = findPixelsInPosition(padI, idG, 0, 0);
+    J(1:2:end, 2:2:end, idG) = sI(1:2:end, 2:2:end);
+    J(2:2:end, 1:2:end, idG) = sI(2:2:end, 1:2:end);
+    J(2:2:end, 2:2:end, idG) = findPixelsInPosition(padI, idG, 1, 1);
+
+
+    %% Build the blue channel
+    J(1:2:end, 1:2:end, idB) = findPixelsInPosition(padI, idB, 0, 0);
+    J(1:2:end, 2:2:end, idB) = findPixelsInPosition(padI, idB, 0, 1);
+    J(2:2:end, 1:2:end, idB) = findPixelsInPosition(padI, idB, 1, 0);
+    J(2:2:end, 2:2:end, idB) = sI(2:2:end, 2:2:end);
+
+
+    %% Treat boundaries
+    if ~isfield(opts, "treatBoundaries") || opts.treatBoundaries == true
+        newOpts = opts;
+        newOpts.treatBoundaries = false;
+        newOpts.castBack = false;
+        wI = demosaicMalvar2004(ones(size(I)), newOpts);
+        J = J./wI;
     end
 
-    J = cast(round(J), class(I));
-end
-
-function color = pixelRawColorId(r, c)
-    % Channel indices
-    idR = 1;
-    idG = 2;
-    idB = 3;
-
-    mr = mod(r - 1, 2);
-    mc = mod(c - 1, 2);
-
-    if mr == 0 && mc == 0
-        color = idR;
-    elseif mr == 1 && mc == 1
-        color = idB;
-    else
-        color = idG;
+    if ~isfield(opts, "castBack") || opts.castBack == true
+        J = cast(J, class(I));
     end
 end
 
-function weights = getWeights(colorId, r, c)
+function kernel = getKernel(colorId, mr, mc)
     % Channel indices
     idR = 1;
     idG = 2;
     idB = 3;
-
-    mr = mod(r - 1, 2);
-    mc = mod(c - 1, 2);
 
     if mr == 0 && mc == 0 && colorId == idG...
-            || mr == 1 && mc == 1 && colorId == idG 
-        weights = [  0    0   -1    0    0
-                     0    0    2    0    0
-                    -1    2    4    2   -1
-                     0    0    2    0    0
-                     0    0   -1    0    0];
+            || mr == 1 && mc == 1 && colorId == idG
+        kernel = [  0    0   -1    0    0
+                    0    0    2    0    0
+                   -1    2    4    2   -1
+                    0    0    2    0    0
+                    0    0   -1    0    0]/8;
     elseif mr == 0 && mc == 1 && colorId == idR...
             || mr == 1 && mc == 0 && colorId == idB
-        weights = [  0    0  0.5    0    0
-                     0   -1    0   -1    0
-                    -1    4    5    4   -1
-                     0   -1    0   -1    0
-                     0    0  0.5    0    0];
+        kernel = [  0    0  0.5    0    0
+                    0   -1    0   -1    0
+                   -1    4    5    4   -1
+                    0   -1    0   -1    0
+                    0    0  0.5    0    0]/8;
     elseif mr == 1 && mc == 0 && colorId == idR...
             || mr == 0 && mc == 1 && colorId == idB
-        weights = [  0    0   -1    0    0
-                     0   -1    4   -1    0
-                   0.5    0    5    0  0.5
-                     0   -1    4   -1    0
-                     0    0   -1    0    0];
+        kernel = [  0    0   -1    0    0
+                    0   -1    4   -1    0
+                  0.5    0    5    0  0.5
+                    0   -1    4   -1    0
+                    0    0   -1    0    0]/8;
     elseif mr == 0 && mc == 0 && colorId == idB...
             || mr == 1 && mc == 1 && colorId == idR
-        weights = [  0    0 -1.5    0    0
+        kernel = [  0    0 -1.5    0    0
                      0    2    0    2    0
                   -1.5    0    6    0 -1.5
                      0    2    0    2    0
-                     0    0 -1.5    0    0];
+                     0    0 -1.5    0    0]/8;
     else
         error("Unexpected error");
     end
+
+    % Flip for convolution
+    kernel = rot90(kernel, 2);
 end
 
-function boolean = isWithinBounds(r, c, h, w)
-    boolean = 1 <= r && r <= h && 1 <= c && c <= w;
+function pixels = findPixelsInPosition(sI, colorId, mr, mc)
+    pixels = convn(sI, getKernel(colorId, mr, mc), "valid");
+    pixels = pixels((mr+1):2:end, (mc+1):2:end);
 end
