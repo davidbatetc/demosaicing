720e5e703d6aefb20b26b2e80d17eb936ab3a9ca
davidbatetc <davidbatetc+github@gmail.com>
2023-04-15 19:08:50 +0300

    Remove hardcoded type uint8 in costFunctionLinear5x5

--- costFunctionLinear5x5.m
+++ costFunctionLinear5x5.m
@@ -8,7 +8,7 @@ function cost = costFunctionLinear5x5(Is, N, weightsSigma)
     w = width(Is);
     n_images = size(Is, 4);
 
-    Js = zeros([h, w, 3, n_images], "uint8");
+    Js = zeros([h, w, 3, n_images], class(Is));
     for idx = 1:n_images
         mI = immosaic(Is(:, :, :, idx), opts.sensorAlignment);
         Js(:, :, :, idx) = demosaicLinear5x5(mI, opts);
